8549f40ef7631f02e1320e023f8e1dd5c3af382e
iandioch <iandioch11@gmail.com>
2017-04-01 04:39:50 +0100

    make pep8 compliant, add stop word check

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import csv
 import random
 import time
 import tweepy
-from urllib import request
+from urllib import request, parse
 
 AUTH_FILE = 'twitter_auth.hidden'
 CHART_URL = 'https://spotifycharts.com/regional/global/daily/latest/download'
 THEMED_WORDS_FILE = 'twitter_words.txt'
+STOP_WORDS_FILE = 'stop_words.txt'
 # number of minutes between tweets
-TWEET_INTERVAL = 1
+TWEET_INTERVAL = 15
 # number of tweets to send before refreshing the track list
 TWEETS_BEFORE_CHART_UPDATE = (24*60)//TWEET_INTERVAL
+
 
 def download_latest_chart():
     """Returns a list of (track, artist) tuples"""
@@ -18,46 +20,76 @@ def download_latest_chart():
         req = request.urlopen(CHART_URL)
         chart = req.read().decode('utf-8').split('\n')
         reader = csv.reader(chart)
-        next(reader) # skip the titles of columns
+        next(reader)  # skip the titles of columns
         tracks = [(row[1], row[2]) for row in reader if len(row) > 0]
         return tracks
     except Exception as e:
         print('Error fetching chart:', e)
         return None
 
-def load_themed_words():
+
+def load_file(path):
     try:
         with open(THEMED_WORDS_FILE, 'r') as f:
-            words = [word.strip() for word in f.readlines()]
+            words = [word.lower().strip() for word in f.readlines()]
             return words
     except Exception as e:
-        print('Error loading themed word file:', e)
+        print('Error loading file "{}": "{}"'.format(path, e))
         return None
 
-def generate_tweet(tracks, themed_words):
+
+def load_themed_words():
+    return load_file(THEMED_WORDS_FILE)
+
+
+def load_stop_words():
+    return load_file(STOP_WORDS_FILE)
+
+
+def replace_non_stop_word(words, theme_word, stop_words):
+    for _ in range(len(words)):
+        n = random.randrange(len(words))
+        if words[n] not in stop_words:
+            words[n] = theme_word
+            return ' '.join(words)
+    return None
+
+
+def generate_tweet(tracks, themed_words, stop_words):
     track, artist = random.choice(tracks)
+    track = track.lower()
+    artist = artist.lower()
     theme_word = random.choice(themed_words)
-    track_words = track.lower().split(' ')
-    artist_words = artist.lower().split(' ')
+    track_words = track.split(' ')
+    artist_words = artist.split(' ')
     if len(track_words) == 1:
         # no good
         if len(artist_words) == 1:
             # this artist + track combo isn't useful, try another
             # this is very elegant code, don't @ me
-            return generate_tweet(tracks, themed_words)
-        n = random.randrange(len(artist_words))
-        artist_words[n] = theme_word
+            return generate_tweet(tracks, themed_words, stop_words)
+
+        # can use artist name
+        a = replace_non_stop_word(artist_words, theme_word, stop_words)
+        if a is None:
+            return generate_tweet(tracks, themed_words, stop_words)
+        artist = a
     else:
-        n = random.randrange(len(track_words))
-        track_words[n] = theme_word
-    return '"{}" - {}'.format(' '.join(track_words), ' '.join(artist_words))
+        # can use track name (preferred)
+        t = replace_non_stop_word(track_words, theme_word, stop_words)
+        if t is None:
+            return generate_tweet(tracks, themed_words, stop_words)
+        track = t
+    return "'{}' - {}".format(track, artist)
+
 
 def tweet(api, message):
     print('Tweeting:', message)
     try:
-        api.update_status(message)
+        api.update_status(status=message)
     except Exception as e:
-        print('Failure during tweet:', e)
+        print('Failure during tweet:', e, type(e))
+
 
 def main():
     api = None
@@ -75,15 +107,22 @@ def main():
         print('themed words is None')
         return
 
+    stop_words = load_stop_words()
+    if stop_words is None:
+        print('stop words is None')
+        return
+    stop_words = set(stop_words)
+
     while True:
         for _ in range(TWEETS_BEFORE_CHART_UPDATE):
             tracks = download_latest_chart()
             if tracks is None:
                 time.sleep(TWEET_INTERVAL*60)
                 break
-            message = generate_tweet(tracks, themed_words)
+            message = generate_tweet(tracks, themed_words, stop_words)
             tweet(api, message)
             time.sleep(TWEET_INTERVAL*60)
+
 
 if __name__ == '__main__':
     main()
